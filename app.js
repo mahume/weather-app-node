a369b5192a71172838fb5b77c558117b64ffe244
mahume <michael.a.hume@gmail.com>
2019-09-01 16:31:52 -0600

    Adding geoweather error handling

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const request = require('request');
 require('dotenv').config();
 
 // Geocoding
-const geocodeURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${process.env.MAPBOX_KEY}`
-request({
-  url: geocodeURL,
-  json: true,
-}, (error, response) => {
-  const { center: [ lng, lat ] } = response.body.features[0];
-  console.log(lat, lng);
+// const geocodeURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${process.env.MAPBOX_KEY}`
+// request({
+//   url: geocodeURL,
+//   json: true,
+// }, (error, response) => {
+//   const { center: [ lng, lat ] } = response.body.features[0];
+//   console.log(lat, lng);
   
-})
+// })
 
 // Weather
 const weatherURL = `https://api.darksky.net/forecast/${process.env.DARK_SKY_KEY}/37.8267,-122.4233`
@@ -19,10 +19,16 @@ request({
   url: weatherURL,
   json: true,
 }, (error, response) => {
-  const { temperature, precipProbability } = response.body.currently;
-  const { summary } = response.body.daily.data[0];
-  console.log(`
-  Today's forecast: ${summary}
-  It is currently ${temperature}° out. There is a ${precipProbability}% chance of rain.
-  `);
+  if (error) {
+    console.log('Unable to connect to weather server.');
+  } else if (response.body.error) {
+    console.log('Unable to find location.');
+  } else {
+    const { temperature, precipProbability } = response.body.currently;
+    const { summary } = response.body.daily.data[0];
+    console.log(`
+    Today's forecast: ${summary}
+    It is currently ${temperature}° out. There is a ${precipProbability}% chance of rain.
+    `);
+  }
 })
