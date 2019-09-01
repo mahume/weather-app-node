54eb184a6febb082feae751e03cba18aa510e6a1
mahume <michael.a.hume@gmail.com>
2019-09-01 16:25:59 -0600

    Adding Geocoding API

--- app.js
+++ app.js
@@ -2,13 +2,22 @@ const request = require('request');
 require('dotenv').config();
 
 // Geocoding
+const geocodeURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${process.env.MAPBOX_KEY}`
+request({
+  url: geocodeURL,
+  json: true,
+}, (error, response) => {
+  const { center: [ lng, lat ] } = response.body.features[0];
+  console.log(lat, lng);
+  
+})
 
-
-const url = `https://api.darksky.net/forecast/${process.env.DARK_SKY_KEY}/37.8267,-122.4233`
+// Weather
+const weatherURL = `https://api.darksky.net/forecast/${process.env.DARK_SKY_KEY}/37.8267,-122.4233`
 
 request({ 
-  url, 
-  json: true 
+  url: weatherURL,
+  json: true,
 }, (error, response) => {
   const { temperature, precipProbability } = response.body.currently;
   const { summary } = response.body.daily.data[0];
